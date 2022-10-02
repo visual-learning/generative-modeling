106583209558033b8859d0c19f1f0985c34142b4
Anirudh Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2022-10-02 16:30:31 -0400

    Add Unet and utils

---
+++ diffusion/utils.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torchvision.utils import make_grid
+
+
+def cosine_beta_schedule(timesteps, s=0.008):
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps, dtype=torch.float32)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * np.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, 0.999)
+
+
+def save_samples(samples, fname, nrow=6, title='Samples'):
+    grid_img = make_grid(samples, nrow=nrow)
+   
+    plt.title(title)
+    plt.imshow(grid_img.permute(1, 2, 0))
+    plt.tight_layout()
+    plt.savefig(fname)
