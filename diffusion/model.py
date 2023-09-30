d812dc87756ecd332d43294d8e0147e1f668f610
Wen-Hsuan Chu <47765488+wenhsuanchu@users.noreply.github.com>
2023-09-30 18:53:36 -0400

    Update model.py

--- diffusion/model.py
+++ diffusion/model.py
@@ -29,25 +29,37 @@ class DiffusionModel(nn.Module):
         self.num_timesteps = self.betas.shape[0]
 
         alphas = 1. - self.betas
-        # TODO 3.1: compute the cumulative products for current and previous timesteps
-        self.alphas_cumprod = ...
-        self.alphas_cumprod_prev =  ...
+        ##################################################################
+        # TODO 3.1: Compute the cumulative products for current and
+        # previous timesteps.
+        ##################################################################
+        self.alphas_cumprod = None
+        self.alphas_cumprod_prev =  None
 
-        # TODO 3.1: pre-compute values needed for forward process
+        ##################################################################
+        # TODO 3.1: Pre-compute values needed for forward process.
+        ##################################################################
         # This is the coefficient of x_t when predicting x_0
-        self.x_0_pred_coef_1 = ...
+        self.x_0_pred_coef_1 = None
         # This is the coefficient of pred_noise when predicting x_0
-        self.x_0_pred_coef_2 = ...
+        self.x_0_pred_coef_2 = None
 
-        # TODO 3.1: compute the coefficients for the mean
+        ##################################################################
+        # TODO 3.1: Compute the coefficients for the mean.
+        ##################################################################
         # This is coefficient of x_0 in the DDPM section
-        self.posterior_mean_coef1 = ...
+        self.posterior_mean_coef1 = None
         # This is coefficient of x_t in the DDPM section
-        self.posterior_mean_coef2 = ...
+        self.posterior_mean_coef2 = None
 
-        # TODO 3.1: compute posterior variance
-        # calculations for posterior q(x_{t-1} | x_t, x_0) in DDPM
-        self.posterior_variance = ...
+        ##################################################################
+        # TODO 3.1: Compute posterior variance.
+        ##################################################################
+        # Calculations for posterior q(x_{t-1} | x_t, x_0) in DDPM
+        self.posterior_variance = None
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
         self.posterior_log_variance_clipped = torch.log(
             self.posterior_variance.clamp(min =1e-20))
 
@@ -59,23 +71,44 @@ class DiffusionModel(nn.Module):
         self.ddim_sampling_eta = ddim_sampling_eta
 
     def get_posterior_parameters(self, x_0, x_t, t):
-        # TODO 3.1: Compute the posterior mean and variance for x_{t-1}
-        # using the coefficients, x_t, and x_0
-        # hint: can use extract function from utils.py
+        # Compute the posterior mean and variance for x_{t-1}
+        # using the coefficients, x_t, and x_0.
+        posterior_mean = (
+            extract(self.posterior_mean_coef1, t, x_t.shape) * x_0 +
+            extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+        )
+        posterior_variance = extract(self.posterior_variance, t, x_t.shape)
+        posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, x_t.shape)
         return posterior_mean, posterior_variance, posterior_log_variance_clipped
 
     def model_predictions(self, x_t, t):
-        # TODO 3.1: given a noised image x_t, predict x_0 and the additive noise
-        # to predict the additive noise, use the denoising model.
-        # Hint: You can use extract function from utils.py.
-        # clamp x_0 to [-1, 1]
+        ##################################################################
+        # TODO 3.1: Given a noised image x_t, predict x_0 and the additive
+        # noise to predict the additive noise, use the denoising model.
+        # Hint: You can use extract function from utils.py. See
+        # get_posterior_parameters() for usage examples.
+        ##################################################################
+        pred_noise = None
+        x_0 = None
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
         return (pred_noise, x_0)
 
     @torch.no_grad()
     def predict_denoised_at_prev_timestep(self, x, t: int):
-        # TODO 3.1: given x at timestep t, predict the denoised image at x_{t-1}.
-        # also return the predicted starting image.
-        # Hint: To do this, you will need a predicted x_0. Which function can do this for you?
+        ##################################################################
+        # TODO 3.1: Given x at timestep t, predict the denoised image at
+        # x_{t-1}, and return the predicted starting image.
+        # noise to predict the additive noise, use the denoising model.
+        # Hint: To do this, you will need a predicted x_0. You should've
+        # already implemented a function to give you x_0 above!
+        ##################################################################
+        pred_img = None
+        x_0 = None
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
         return pred_img, x_0
 
     @torch.no_grad()
@@ -88,26 +121,35 @@ class DiffusionModel(nn.Module):
         return img
 
     def sample_times(self, total_timesteps, sampling_timesteps):
-        # TODO 3.2: Generate a list of times to sample from.
-        pass
+        times = torch.linspace(-1, total_timesteps - 1, steps=sampling_timesteps + 1)
+        return list(reversed(times.int().tolist()))
 
     def get_time_pairs(self, times):
-        # TODO 3.2: Generate a list of adjacent time pairs to sample from.
-        pass
+        return list(zip(times[:-1], times[1:]))
 
     def ddim_step(self, batch, device, tau_i, tau_isub1, img, model_predictions, alphas_cumprod, eta):
-        # TODO 3.2: Compute the output image for a single step of the DDIM sampling process.
+        ##################################################################
+        # TODO 3.2: Compute the output image for a single step of the DDIM
+        # sampling process.
+        ##################################################################
+        # Step 1: Predict x_0 and the additive noise for tau_i
+        x_0 = None
 
-        # predict x_0 and the additive noise for tau_i
+        # Step 2: Extract \alpha_{\tau_{i - 1}} and \alpha_{\tau_{i}}
+        pass
 
-        # extract \alpha_{\tau_{i - 1}} and \alpha_{\tau_{i}}
+        # Step 3: Compute \sigma_{\tau_{i}}
+        pass
 
-        # compute \sigma_{\tau_{i}}
+        # Step 4: Compute the coefficient of \epsilon_{\tau_{i}}
+        pass
 
-        # compute the coefficient of \epsilon_{\tau_{i}}
-
-        # sample from q(x_{\tau_{i - 1}} | x_{\tau_t}, x_0)
-        # HINT: use the reparameterization trick
+        # Step 5: Sample from q(x_{\tau_{i - 1}} | x_{\tau_t}, x_0)
+        # HINT: Use the reparameterization trick
+        img = None
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
 
         return img, x_0
 
@@ -132,5 +174,6 @@ class DiffusionModel(nn.Module):
 
     @torch.no_grad()
     def sample_given_z(self, z, shape):
-        #TODO 3.3: fill out based on the sample function above
-        pass
+        sample_fn = self.sample_ddpm if not self.is_ddim_sampling else self.sample_ddim
+        z = z.reshape(shape)
+        return sample_fn(shape, z)
